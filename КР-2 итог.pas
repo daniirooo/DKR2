1e883a60b06cdee99ac8cc63a5d57d212e07d6b4
daniirooo <117731362+daniirooo@users.noreply.github.com>
2022-11-08 16:48:11 +0300

    Add files via upload

---
+++ КР-2 итог.pas
@@ -0,0 +1,16 @@
+﻿begin
+   var s: Integer;
+   var sr: real; 
+   var a: array [1..9999] of integer;
+var n := ReadInteger;
+for var i := 1 to n do
+begin
+a[i]:=random(-100,100);
+print(a[i]);
+end;
+println();
+for var i := 1 to n do
+s := s + a[i];
+sr := s / n;
+println(sr);
+end.
